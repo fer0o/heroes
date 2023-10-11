ba63261331ee580a17ba5b23af474f4563486b61
fer0o <fer0osennin@gmail.com>
2023-10-11 11:17:38 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const allTabsBody = document.querySelectorAll('.tab-body-single');
+const allTabsHead = document.querySelectorAll('.tab-head-single');
+
+let activeTab = 1, allData;
+
+const init = () => {
+    showActiveTabBody();
+    showActiveTabHead();
+}
+
+const showActiveTabHead = () => allTabsHead[activeTab - 1].classList.add('active-tab'); 
+
+
+const showActiveTabBody = () => {
+    hideAllTabBody();
+    allTabsBody[activeTab - 1].classList.add('show-tab');
+}
+
+const hideAllTabBody = () => allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove('show-tab'));
+const hideAllTabHead = () => allTabsHead.forEach(singleTabHead => singleTabHead.classList.remove('active-tab'));
+
+// even listeners
+window.addEventListener('DOMContentLoaded', () => init());
+// button event listeners
+allTabsHead.forEach(singleTabHead => {
+    singleTabHead.addEventListener('click', () => {
+        hideAllTabHead();
+        activeTab = singleTabHead.dataset.id;
+        showActiveTabHead();
+        showActiveTabBody();
+    });
+});
